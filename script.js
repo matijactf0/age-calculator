d991d750c19bf5b91ef418ee9446071f365da1aa
matijactf0 <147106846+matijactf0@users.noreply.github.com>
2023-10-05 23:52:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateAge() {
+    var dobInput = document.getElementById("dob").value;
+    var dob = new Date(dobInput);
+    var currentDate = new Date();
+    var ageInMillis = currentDate - dob;
+    var ageInYears = ageInMillis / (1000 * 60 * 60 * 24 * 365.25);
+    ageInYears = Math.floor(ageInYears);
+
+    var resultContainer = document.getElementById("result-container");
+    var resultElement = document.getElementById("result");
+    var loader = document.querySelector(".loader");
+
+    loader.style.display = "block";
+
+    setTimeout(function() {
+        loader.style.display = "none";
+        resultElement.innerHTML = "Vaša starost je: " + ageInYears + " godina.";
+    }, 1000); // Simulacija procesa koji traje 1 sekundu
+}
